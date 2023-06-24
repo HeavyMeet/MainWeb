88d81f23450e983e79b940588260aba4e0124f9e
HeavyMeet <ericklinkes@gmail.com>
2023-06-23 23:15:19 -0600

    6th commit

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useContext} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { ThemeContext }  from '../context/ThemeProvider'
 
 const Presentation = () => {
  
   const { theme } = useContext(ThemeContext)
-
+  const userAgent = navigator.userAgent;
 useEffect(() => {
  function back(){
    let div = document.querySelector('#in');
@@ -43,56 +43,56 @@ useEffect(() => {
      div.append(drop);
    }
  }
-//  back(); 
+ if (!(/Mobi/.test(userAgent))) 
+   back(); 
 },[theme]);
-
- const generatePublicUrl = async () => {
-   
-  return (
-    <div>
-      <iframe
-      src='https://drive.google.com/file/d/0B9HNz7UaBvXOc3RhcnRlcl9maWxl/preview'
-      width="640"
-      height="480"
-      allow="autoplay"
-      title="pdf"
-      />
-    </div>
-  )
-  }
 
   return (
     <>
-      {/* <div>
-        <h1 className="span1" style={{color:theme === "dark" ? '#9abddf':'#1f1919',textShadow:theme?'12px 12px 3px rgb(63, 84, 105)':'12px 12px 3px rgba(89, 72, 72, 0.6)'}} data-scroll>
-        <span data-scroll data-scroll-delay='0.15' data-scroll-speed='5'>E</span>
-        <span data-scroll data-scroll-delay='0.095' data-scroll-speed='5'>r</span>
-        <span data-scroll data-scroll-delay='0.085' data-scroll-speed='5'>i</span>
-        <span data-scroll data-scroll-delay='0.075' data-scroll-speed='5'>c</span>
-        <span data-scroll data-scroll-delay='0.065' data-scroll-speed='5'>k</span>
-        </h1>
-        <h1 className="span2" style={{color:theme === "dark" ? '#9abddf':'#1f1919',textShadow:theme?'12px 12px 3px rgb(63, 84, 105)':'12px 12px 3px rgba(89, 72, 72, 0.6)'}} data-scroll>
-        <span data-scroll data-scroll-delay='0.15' data-scroll-speed='5'></span>
-        <span data-scroll data-scroll-delay='0.095' data-scroll-speed='5'>R</span>
-        <span data-scroll data-scroll-delay='0.085' data-scroll-speed='5'>e</span>
-        <span data-scroll data-scroll-delay='0.075' data-scroll-speed='5'>a</span>
-        <span data-scroll data-scroll-delay='0.065' data-scroll-speed='5'>l</span>
-        </h1>
-        </div>
-        <div>
-          <h1 style={{ padding:'0', margin:'0'}}>
-        <span className='span3' data-scroll data-scroll-direction="vertical" data-scroll-delay="0.05" data-scroll-speed="5">
-          Web & Mobile Developer
-        </span>
-        </h1>
-        </div>
-        <div>
-        <h1 style={{padding:'0', margin:'70px 0px 0 10px'}}>
-        <span className='span4' data-scroll data-scroll-direction="vertical" data-scroll-delay="0.05" data-scroll-speed="5">
-             <a href='https://drive.google.com/file/d/1Y_PhKCNzokQLB0OIQCIYpA4PQScGjIsL/view?usp=sharing' target="_blank">My CV</a>
-             </span>
-        </h1>
-        </div>    */}
+    {/Mobi/.test(userAgent) === false ?   
+      <>
+      <div>
+          <h1 className="span1" style={{ color: theme === "dark" ? '#9abddf' : '#1f1919', textShadow: theme ? '12px 12px 3px rgb(63, 84, 105)' : '12px 12px 3px rgba(89, 72, 72, 0.6)' }} data-scroll>
+            <span data-scroll data-scroll-delay='0.15' data-scroll-speed='5'>E</span>
+            <span data-scroll data-scroll-delay='0.095' data-scroll-speed='5'>r</span>
+            <span data-scroll data-scroll-delay='0.085' data-scroll-speed='5'>i</span>
+            <span data-scroll data-scroll-delay='0.075' data-scroll-speed='5'>c</span>
+            <span data-scroll data-scroll-delay='0.065' data-scroll-speed='5'>k</span>
+          </h1>
+          <h1 className="span2" style={{ color: theme === "dark" ? '#9abddf' : '#1f1919', textShadow: theme ? '12px 12px 3px rgb(63, 84, 105)' : '12px 12px 3px rgba(89, 72, 72, 0.6)' }} data-scroll>
+            <span data-scroll data-scroll-delay='0.15' data-scroll-speed='5'></span>
+            <span data-scroll data-scroll-delay='0.095' data-scroll-speed='5'>R</span>
+            <span data-scroll data-scroll-delay='0.085' data-scroll-speed='5'>e</span>
+            <span data-scroll data-scroll-delay='0.075' data-scroll-speed='5'>a</span>
+            <span data-scroll data-scroll-delay='0.065' data-scroll-speed='5'>l</span>
+          </h1>
+        </div><div>
+            <h1 style={{ padding: '0', margin: '0' }}>
+              <span className='span3' data-scroll data-scroll-direction="vertical" data-scroll-delay="0.05" data-scroll-speed="5">
+                Web & Mobile Developer
+              </span>
+            </h1>
+          </div><div>
+            <h1 style={{ padding: '0', margin: '70px 0px 0 10px' }}>
+              <span className='span4' data-scroll data-scroll-direction="vertical" data-scroll-delay="0.05" data-scroll-speed="5">
+                <a href='https://drive.google.com/file/d/1Y_PhKCNzokQLB0OIQCIYpA4PQScGjIsL/view?usp=sharing' target="_blank">My CV</a>
+              </span>
+            </h1>
+          </div></>
+        : <>
+        <h2 className="span1" style={{ color: theme === "dark" ? '#9abddf' : '#1f1919', textShadow: theme ? '12px 12px 3px rgb(63, 84, 105)' : '12px 12px 3px rgba(89, 72, 72, 0.6)', fontSize:'50px' }}>
+          <span>Erick Real</span>
+        </h2>
+        <h3 className="span1" style={{ color: theme === "dark" ? '#9abddf' : '#1f1919', textShadow: theme ? '12px 12px 3px rgb(63, 84, 105)' : '12px 12px 3px rgba(89, 72, 72, 0.6)', fontSize:'40px' }}>
+            <span>Web & Mobile Developer</span>
+          </h3>
+          <h4 style={{ padding: '0', margin: '70px 0px 0 10px' }}>
+              <span style={{fontSize:'25px'}}>
+                <a href='https://drive.google.com/file/d/1Y_PhKCNzokQLB0OIQCIYpA4PQScGjIsL/view?usp=sharing' target="_blank">My CV</a>
+              </span>
+            </h4>
+          </> 
+       }  
       </>
       )
 }
